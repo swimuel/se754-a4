c5d669a2e659a92b27504178330935dd5efbd7de
Brevellnash <brevellnash@gmail.com>
2019-05-09 11:54:55 +1200

    changed gitignore again, added some tests and updated yml file to run gradle build

---
+++ src/test/java/TestGitHubIntegration.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestGitHubIntegration {
+
+    @Test
+    public void testSignIn() {
+        fail();
+    } 
+    
+    @Test
+    public void testSignOut() {
+        fail();
+    }
+
+    @Test 
+    public void testPullRequestFetch() {
+        fail();
+    }
+    
+    @Test
+    public void testMerge() {
+        fail();
+    }
+
+    @Test
+    public void testPostComment() {
+        fail();
+    }
+
+}

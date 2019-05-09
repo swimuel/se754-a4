943d5c4a60ac5aea685a99ceff09715242f9ee2a
Brevellnash <brevellnash@gmail.com>
2019-05-09 14:35:54 +1200

    added a very basic idea of siging in, updated the tests associated to this

--- src/main/java/GitHubIntegration.java
+++ src/main/java/GitHubIntegration.java
@@ -1,36 +1,52 @@
 import java.io.IOException;
 
 import org.eclipse.egit.github.core.Repository;
-import org.eclipse.egit.github.core.client.*;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
 public class GitHubIntegration {
-
 
     private String _username;
     private String _password;
 
     public GitHubIntegration() {
-
+        _username = null;
+        _password = null;
     }
 
-    public GitHubIntegration(String username, String password) {
+
+    /**
+     * Sets the fields for that user so that they can be used to perform other tasks
+     * 
+     * @param username username fo the user
+     * @param password user password
+     */
+    public void signIn(String username, String password) {
         _username = username;
         _password = password;
     }
 
 
-    public static void main(String [] args) {
+    /**
+     * Removes the username and password so that any further user actions 
+     * will fail
+     */
+    public void signOut() {
+        _username = null;
+        _password = null;
+    }
 
+
+
+    public static void main(String [] args) {
 
         // GitHubClient client = new GitHubClient();
         // client.setOAuth2Token("d7ee4fc6478de24e8985f747edf4b71f6df01377");
 
         // System.out.println(client.getUser());
+
         RepositoryService service = new RepositoryService();
-        service.getClient().setOAuth2Token("d7ee4fc6478de24e8985f747edf4b71f6df01377");
         try {
-            for (Repository repo : service.getRepositories())
+            for (Repository repo : service.getRepositories("swimuel"))
                 System.out.println(repo.getName());
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -39,6 +55,12 @@ public class GitHubIntegration {
     }
 
 
-
+    /**
+     * Gets the username 
+     * @return String username
+     */
+    public String getUsername() {
+        return _username;
+    }
 
 }
--- src/test/java/TestGitHubIntegration.java
+++ src/test/java/TestGitHubIntegration.java
@@ -5,12 +5,22 @@ public class TestGitHubIntegration {
 
     @Test
     public void testSignIn() {
-        fail();
+        GitHubIntegration user = new GitHubIntegration();
+        user.signIn("username", "password");
+        // check username has been stored
+        assertEquals(user.getUsername(), "username");
     } 
     
     @Test
     public void testSignOut() {
-        fail();
+        GitHubIntegration user = new GitHubIntegration();
+        // need to be signed in to sign out 
+        user.signIn("username", "password");
+        // sign out 
+        user.signOut();
+        String username = user.getUsername();
+        // Check username is no longer stored
+        assertEquals(username, null);
     }
 
     @Test 

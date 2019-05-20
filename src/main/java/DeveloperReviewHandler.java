e5264ff40b4ef005d35ee62da20d41fbb7e84e16
Darcy Cox <darcycox97@gmail.com>
2019-05-21 09:33:05 +1200

    Green: random allocation tests passing when random returns extreme values

--- src/main/java/DeveloperReviewHandler.java
+++ src/main/java/DeveloperReviewHandler.java
@@ -1,5 +1,12 @@
 import user.Reviewer;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * Defines the actions that developers can perform on a review.
  * Also manages persistence of the review in the database
@@ -29,7 +36,58 @@ public class DeveloperReviewHandler {
         return success;
     }
 
-    public void allocateRandomReviewer() {
+    public Reviewer allocateRandomReviewer(Random random) throws UnauthorizedActionException {
+        List<Reviewer> reviewers = db.getReviewers();
 
+        // sort them from highest count to lowest count.
+        Collections.sort(reviewers, new Comparator<Reviewer>() {
+            public int compare(Reviewer r1, Reviewer r2) {
+                return r2.getReviewCount() - r1.getReviewCount();
+            }
+        });
+
+        Map<Reviewer, Integer> reviewerWeighting = new HashMap();
+        int sumOfWeights = 0;
+        int weight = 1;
+        int prevCount = -1;
+        for (int i = 0; i < reviewers.size(); i++) {
+            Reviewer r = reviewers.get(i);
+
+            // weight does not increase if review count is same as previous element
+            // or if this is the first iteration.
+            if (prevCount != -1 && r.getReviewCount() != prevCount) {
+                weight++;
+            }
+
+            reviewerWeighting.put(r, weight);
+            sumOfWeights += weight;
+            prevCount = r.getReviewCount();
+        }
+
+        // calculate range between 0 and 1 for each reviewer.
+        // find the reviewer index by finding the first ranges value that the random
+        // value is less than.
+        double[] ranges = new double[reviewers.size()];
+        for (int i = 0; i < ranges.length; i++) {
+            int currentWeight = reviewerWeighting.get(reviewers.get(i));
+            double rangeVal = (double) currentWeight / (double) sumOfWeights;
+            if (i != 0) {
+                rangeVal += ranges[i - 1];
+            }
+
+            ranges[i] = rangeVal;
+        }
+
+        double rand = random.nextDouble();
+        Reviewer toAllocate = null;
+        for (int i = 0; i < ranges.length; i++) {
+            if (rand < ranges[i]) {
+                toAllocate = reviewers.get(i);
+                break;
+            }
+        }
+
+        this.addReviewer(toAllocate);
+        return toAllocate;
     }
 }
--- src/test/java/RandomReviewAllocationTest.java
+++ src/test/java/RandomReviewAllocationTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RandomReviewAllocationTest {
@@ -27,7 +28,7 @@ public class RandomReviewAllocationTest {
 
 
     @Test
-    public void shouldAllocateReviewerWithLowestReviewCountWhenRandomCloseTo1() {
+    public void shouldAllocateReviewerWithLowestReviewCountWhenRandomCloseTo1() throws UnauthorizedActionException {
         Reviewer lowestCount = new Reviewer(UUID.randomUUID(), 1);
 
         List<Reviewer> reviewers = new ArrayList<>();
@@ -39,15 +40,32 @@ public class RandomReviewAllocationTest {
         Mockito.doReturn(reviewers).when(db).getReviewers();
         Mockito.doReturn(0.9999).when(random).nextDouble();
 
-        rh.allocateRandomReviewer();
+        Reviewer allocated = rh.allocateRandomReviewer(random);
 
         Mockito.verify(db, Mockito.atLeastOnce()).getReviewers();
         Mockito.verify(db, Mockito.times(1)).addReviewer(review, lowestCount);
         assertTrue(review.getReviewers().contains(lowestCount));
+        assertEquals(allocated, lowestCount);
     }
 
     @Test
-    public void shouldAllocatedReviewerWithHighestReviewCountWhenRandomGivesZero() {
+    public void shouldAllocatedReviewerWithHighestReviewCountWhenRandomGivesZero() throws UnauthorizedActionException {
+        Reviewer highestCount = new Reviewer(UUID.randomUUID(), 30);
 
+        List<Reviewer> reviewers = new ArrayList<>();
+        reviewers.add(highestCount);
+        reviewers.add(new Reviewer(UUID.randomUUID(), 10));
+        reviewers.add(new Reviewer(UUID.randomUUID(), 20));
+        reviewers.add(new Reviewer(UUID.randomUUID(), 2));
+
+        Mockito.doReturn(reviewers).when(db).getReviewers();
+        Mockito.doReturn(0.0).when(random).nextDouble();
+
+        Reviewer allocated = rh.allocateRandomReviewer(random);
+
+        Mockito.verify(db, Mockito.atLeastOnce()).getReviewers();
+        Mockito.verify(db, Mockito.times(1)).addReviewer(review, highestCount);
+        assertTrue(review.getReviewers().contains(highestCount));
+        assertEquals(allocated, highestCount);
     }
 }

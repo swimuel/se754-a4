48bab9a5c0a0c75b228bee3793a4fadcd36dbd37
Sam Broadhead <sambroadhead@icloud.com>
2019-05-20 12:32:27 +1200

    Add more specific linting tests to cover code conventions

--- src/test/java/CodeInspection/TestAutomatedLinting.java
+++ src/test/java/CodeInspection/TestAutomatedLinting.java
@@ -14,17 +14,33 @@ public class TestAutomatedLinting {
 
     @Test
     public void lintSourceCode(){
-        SourceCode lintedCode = new SourceCode("public class test {}\n");
-        SourceCode originalCode = new SourceCode("public class test  { }");
-        SourceCode receivedLintedCode = linter.lint(originalCode);
-        Assert.assertEquals(lintedCode.getValue(), receivedLintedCode.getValue());
+        SourceCode lintedCode = new SourceCode("class test {}\n");
+        SourceCode originalCode = new SourceCode("class test  { }");
+        SourceCode receivedCode = linter.lint(originalCode);
+        Assert.assertEquals(lintedCode.getValue(), receivedCode.getValue());
     }
 
     @Test
     public void alreadyLintedSourceCode(){
-        SourceCode originalCode = new SourceCode("public class test {}\n");
+        SourceCode originalCode = new SourceCode("class test {\n  int x = 1;\n}\n");
         SourceCode receivedLintedCode = linter.lint(originalCode);
         Assert.assertEquals(originalCode.getValue(), receivedLintedCode.getValue());
+    }
+
+    @Test
+    public void spacesOverTabs(){
+        SourceCode lintedCode = new SourceCode("class test {\n  int x = 0;\n}\n");
+        SourceCode originalCode = new SourceCode("class test {\n\tint x = 0;\n}\n");
+        SourceCode receivedCode = linter.lint(originalCode);
+        Assert.assertEquals(lintedCode.getValue(), receivedCode.getValue());
+    }
+
+    @Test
+    public void spaceBetweenVarAssignment(){
+        SourceCode lintedCode = new SourceCode("class test {\n  int x = 1;\n}\n");
+        SourceCode originalCode = new SourceCode("class test{int x=1;}");
+        SourceCode receivedCode = linter.lint(originalCode);
+        Assert.assertEquals(lintedCode.getValue(), receivedCode.getValue());
     }
 
 }

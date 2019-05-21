3b3957d7079b39f9abc2b235668f68347fe63333
Darcy Cox <darcycox97@gmail.com>
2019-05-21 12:00:40 +1200

    Remove unused files

--- src/main/java/Developer.java
+++
@@ -1,7 +0,0 @@
-public interface Developer {
-    public void sendNonDev(Results results, NonDeveloper nonDeveloper);
-    public void sendAbstraction(Abstraction abstraction, NonDeveloper nonDeveloper);
-    public Results fetchResults();
-    public Feedback fetchFeedback();
-    
-}

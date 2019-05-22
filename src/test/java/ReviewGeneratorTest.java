0698644782eda65663cddb2e137147972c77e1cb
Darcy Cox <darcycox97@gmail.com>
2019-05-22 20:16:31 +1200

    Red: non-compiling test for connecting automated review results with review allocation

---
+++ src/test/java/ReviewGeneratorTest.java
@@ -0,0 +1,20 @@
+import com.google.googlejavaformat.java.FormatterException;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ReviewGeneratorTest {
+    @Test
+    public void generateReviewPerformsAutomatedReviewAndInitialisesReviewHandler() throws FormatterException {
+        SourceCode code = new SourceCode("int x = 9;");
+        InitialReviewResults results = Mockito.mock(InitialReviewResults.class);
+
+        AutomatedCodeHandler ah = Mockito.mock(AutomatedCodeHandler.class);
+        Mockito.when(ah.performAutomatedReview(code)).thenReturn(results);
+
+        ReviewGenerator rg = new ReviewGenerator(SourceCode code);
+        DeveloperReviewHandler reviewHandler = rg.generateReviewHandler();
+
+        Mockito.verify(ah, Mockito.times(1)).performAutomatedReview(code);
+        assertEquals(results, reviewHandler.getReviewResults());
+    }
+}

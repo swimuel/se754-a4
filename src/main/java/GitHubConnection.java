17a5456ae47ee68bd112e66a775c7832edf19f0f
Brevellnash <brevellnash@gmail.com>
2019-05-23 09:26:57 +1200

    GREEN: make the connection an interface

--- src/main/java/GitHubConnection.java
+++ src/main/java/GitHubConnection.java
@@ -1,20 +1,14 @@
-public class GitHubConnection {
- 
-    public GitHubConnection() {
-
-    }
-
+public interface GitHubConnection {
+    
     /**
-     * This method is called from signIn method in the GitHubClient class.
-     * It checks that the username and password are correct
+     * This method is called from signIn method in the GitHubClient class. It checks
+     * that the username and password are correct
      * 
      * @param username string github username
      * @param password string github password
      * @throws BadLoginException if the username and password are incorrect
      */
-    public void authenticateUser(String username, String password) throws BadLoginException {
-
-    }
+    public void authenticateUser(String username, String password) throws BadLoginException;
 
     
 }

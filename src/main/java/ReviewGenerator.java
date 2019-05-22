76ec650e6cbb26805ecee29def13ac9625489723
Darcy Cox <darcycox97@gmail.com>
2019-05-22 22:14:25 +1200

    Mock user actions to enable verification that users allocate reviewers when a review is being generated

--- src/main/java/ReviewGenerator.java
+++ src/main/java/ReviewGenerator.java
@@ -2,17 +2,23 @@ import com.google.googlejavaformat.java.FormatterException;
 import user.Developer;
 
 public class ReviewGenerator {
+    UserAction userAction;
     AutomatedCodeHandler ah;
     Database db;
 
-    public ReviewGenerator(AutomatedCodeHandler ah, Database db) {
+    public ReviewGenerator(AutomatedCodeHandler ah, Database db, UserAction userAction) {
         this.ah = ah;
         this.db = db;
+        this.userAction = userAction;
     }
 
     public DeveloperReviewHandler generateReviewHandler(SourceCode code, Developer author) throws FormatterException{
         InitialReviewResults results = ah.performAutomatedReview(code);
         Review review = new Review(results, author);
         return new DeveloperReviewHandler(review, this.db);
+    }
+
+    public void allocateReviewers(DeveloperReviewHandler rh) {
+        userAction.allocateReviewers(rh);
     }
 }
--- src/test/java/ReviewGeneratorTest.java
+++ src/test/java/ReviewGeneratorTest.java
@@ -1,4 +1,5 @@
 import com.google.googlejavaformat.java.FormatterException;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import user.Developer;
@@ -6,19 +7,39 @@ import user.Developer;
 import static org.junit.Assert.assertEquals;
 
 public class ReviewGeneratorTest {
+
+    AutomatedCodeHandler ah;
+    ReviewGenerator rg;
+    UserAction ua;
+
+    @Before
+    public void setup() {
+        this.ah = Mockito.mock(AutomatedCodeHandler.class);
+        this.ua = Mockito.mock(UserAction.class);
+        this.rg = new ReviewGenerator(ah, Mockito.mock(Database.class), ua);
+    }
+
     @Test
     public void generateReviewPerformsAutomatedReviewAndInitialisesReviewHandler() throws FormatterException {
         SourceCode code = new SourceCode("int x = 9;");
         Developer author = new Developer();
         InitialReviewResults results = Mockito.mock(InitialReviewResults.class);
 
-        AutomatedCodeHandler ah = Mockito.mock(AutomatedCodeHandler.class);
         Mockito.when(ah.performAutomatedReview(code)).thenReturn(results);
 
-        ReviewGenerator rg = new ReviewGenerator(ah, Mockito.mock(Database.class));
         DeveloperReviewHandler reviewHandler = rg.generateReviewHandler(code, author);
 
         Mockito.verify(ah, Mockito.times(1)).performAutomatedReview(code);
         assertEquals(results, reviewHandler.getReviewResults());
+    }
+
+    @Test
+    public void allocateReviewersCommunicatesWithUserInterface() {
+        Review review = new Review(Mockito.mock(InitialReviewResults.class), new Developer());
+        DeveloperReviewHandler rh = new DeveloperReviewHandler(review, Mockito.mock(Database.class));
+
+        rg.allocateReviewers(rh);
+
+        Mockito.verify(ua, Mockito.times(1)).allocateReviewers(rh);
     }
 }

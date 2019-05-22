67a0f49e5adf9c4af37ac8ad9bf8026d7a21fbb7
Brevellnash <brevellnash@gmail.com>
2019-05-23 08:50:34 +1200

    RED: adding additional tests for sign in and sign out

--- src/test/java/GitHubClientTest.java
+++ src/test/java/GitHubClientTest.java
@@ -14,4 +14,41 @@ public class GitHubClientTest {
         // check username has been stored
         assertEquals("username", user.getUsername());
     }
+
+    @Test(expected = BadLoginException.class)
+    public void shouldThrowExceptionWhenUsernameAndPasswordAreIncorrect() throws BadLoginException {
+        GitHubConnection mockedConnection = Mockito.mock(GitHubConnection.class);
+        Mockito.doThrow(new BadLoginException()).when(mockedConnection).authenticateUser("username", "badPassword");
+
+        GitHubClient user = new GitHubClient(mockedConnection);
+
+        user.signIn("username", "badPassword");
+    }
+
+    @Test
+    public void shouldNotStoreUsernameIfThereIsABadLoginException() {
+        GitHubConnection mockedConnection = Mockito.mock(GitHubConnection.class);
+        GitHubClient user = new GitHubClient(mockedConnection);
+        try {
+            Mockito.doThrow(new BadLoginException()).when(mockedConnection).authenticateUser("username", "badPassword");
+            user.signIn("username", "badPassword");
+        } catch (BadLoginException e) {
+            // check that the username was not stored after the login attempt
+            String username = user.getUsername();
+            assertEquals(null, username);
+        }
+    }
+
+    @Test
+    public void shouldRemoveUsernamePasswordAfterSignOut() throws BadLoginException {
+        GitHubConnection mockedConnection = Mockito.mock(GitHubConnection.class);
+        GitHubClient user = new GitHubClient(mockedConnection);
+        // need to be signed in to sign out
+        user.signIn("username", "password");
+        // sign out
+        user.signOut();
+        String username = user.getUsername();
+        // Check username is no longer stored
+        assertEquals(null, username);
+    }
 }

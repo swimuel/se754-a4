023cab181087c750e7e0a96a7ab21c1c9f8c8f0f
Darcy Cox <darcycox97@gmail.com>
2019-05-23 18:41:48 +1200

    Non-compiling test for verifying feedback can be received from non-dev tool

---
+++ src/test/java/FeedbackHandlerTest.java
@@ -0,0 +1,36 @@
+import com.google.googlejavaformat.java.FormatterException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import user.Developer;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+// ensures developers receive feedback and can approve the review
+// which will then trigger interaction with github
+public class FeedbackHandlerTest {
+    GitHubClient ghc;
+    NonDeveloperConnection ndc;
+    ReviewGenerator rg;
+    FeedbackHandler fh;
+
+    @Before
+    public void setup() {
+        ghc = Mockito.mock(GitHubClient.class);
+        ndc = Mockito.mock(NonDeveloperConnection.class);
+        devFlow = Mockito.mock(DeveloperFlow.class);
+    }
+
+    @Test
+    public void feedbackReceivedFromNonDeveloperToolAndStored()  {
+        Feedback feedback = new Feedback("yes", "ok");
+        Mockito.when(ndc.fetchFeedback()).thenReturn(feedback);
+
+        fh.receiveFeedback();
+        
+        Mockito.verify(ndc, Mockito.times(1)).fetchFeedback();
+        assertEquals(feedback, fh.getFeedback());
+    }
+}

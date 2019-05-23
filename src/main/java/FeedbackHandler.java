f02c919348568345a0e715dff30c8fbd853869c7
Darcy Cox <darcycox97@gmail.com>
2019-05-23 20:18:42 +1200

    Add tests for verifying user interaction with the feedback and posting to github

--- src/main/java/FeedbackHandler.java
+++ src/main/java/FeedbackHandler.java
@@ -1,10 +1,16 @@
 public class FeedbackHandler {
-    NonDeveloperConnection ndc;
-    Feedback feedback;
+    private NonDeveloperConnection ndc;
+    private Feedback feedback;
+    private boolean approvalStatus;
+    private GitHubClient ghc;
+    private UserAction ua;
 
-    public FeedbackHandler(NonDeveloperConnection ndc) {
+    public FeedbackHandler(NonDeveloperConnection ndc, UserAction ua, GitHubClient ghc) {
         this.ndc = ndc;
         this.feedback = null;
+        this.approvalStatus = false;
+        this.ua = ua;
+        this.ghc = ghc;
     }
 
     public void receiveFeedback() {
@@ -13,5 +19,30 @@ public class FeedbackHandler {
 
     public Feedback getFeedback() {
         return this.feedback;
+    }
+
+    public void setFeedback(Feedback feedback) {
+        this.feedback = feedback;
+    }
+
+    public void getDeveloperApproval() {
+        this.approvalStatus = this.ua.getDeveloperApproval(this.feedback);
+    }
+
+    public boolean getApprovalStatus() {
+        return this.approvalStatus;
+    }
+
+    public void setApprovalStatus(boolean approval) {
+        this.approvalStatus = approval;
+    }
+
+    public void finishReview() throws MergeException {
+        // regardless of approval status, we want to post the comments
+        this.ghc.createPullRequestComment(this.feedback.getComments(), "tbc", "tbc", 1);
+
+        if (this.approvalStatus) {
+            this.ghc.mergeChanges("tc", "tbc", 1, "tbc");
+        }
     }
 }
--- src/main/java/UserAction.java
+++ src/main/java/UserAction.java
@@ -1,4 +1,7 @@
 public interface UserAction {
     /** Interacts with the user in order to add reviewers */
     void allocateReviewers(DeveloperReviewHandler rh);
+
+    /** Prompts the developer to approve or reject the pull request based on the reviewer feedback */
+    boolean getDeveloperApproval(Feedback feedback);
 }
